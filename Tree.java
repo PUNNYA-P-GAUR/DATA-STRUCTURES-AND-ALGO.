4a6900a2acfff8b4c761cd66efd5d08c4c19d17f
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2022-03-31 22:53:52 +0530

    Recursive Preorder Traversal

---
+++ Tree.java
@@ -0,0 +1,39 @@
+class BinaryTree {
+    Node root;
+
+   static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree b1 = new BinaryTree();
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        b1.root = n1;
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        b1.preOrder(b1.root);
+    }
+}
