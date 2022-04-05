835b0e42d76c8daaa51c7493c1740d027c248706
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2022-04-05 11:47:38 +0530

    TREE.JAVA
    ALL RECURSIVE TRAVERSAL METHODS ADDED

--- Tree.java
+++ Tree.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 class BinaryTree {
     Node root;
 
@@ -11,29 +13,58 @@ class BinaryTree {
         }
     }
 
-    void preOrder(Node root) {
+    void preOrder(Node root) {                      //RECURSIVE CALL METHOD
         if (root == null) {
             return;
         }
         System.out.print(root.data + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+        preOrder(root.left);                         //RECURSIVE CALL
+        preOrder(root.right);                        //RECURSIVE CALL
+    }
+    void postOrder(Node root){                      //RECURSIVE CALL METHOD
+       if(root==null)
+           return;
+       postOrder(root.left);                       //RECURSIVE CALL
+       postOrder(root.right);                      //RECURSIVE CALL
+       System.out.print(root.data+" ");
+
+    }
+    void inOrder(Node root){                             //RECURSIVE CALL METHOD
+       if(root==null)
+           return;
+       inOrder(root.left);                               //RECURSIVE CALL
+       System.out.print(root.data+" ");
+       inOrder(root.right);                              //RECURSIVE CALL
     }
 
     public static void main(String[] args) {
         BinaryTree b1 = new BinaryTree();
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        Node n5 = new Node(5);
-        Node n6 = new Node(6);
+        Node n1 = new Node(20);
+        Node n2 = new Node(30);
+        Node n3 = new Node(40);
+        Node n4 = new Node(80);
+        Node n5 = new Node(40);
+        Node n6 = new Node(50);
+        Node n7=new Node(60);
+        Node n8 =new Node(90);
+        Node n9=new Node(100);
         b1.root = n1;
         n1.left = n2;
-        n1.right = n3;
-        n2.left = n4;
-        n2.right = n5;
-        n3.left = n6;
+        n1.right=n5;
+        n2.left=n3;
+        n3.right=n4;
+        n5.left=n6;
+        n5.right=n7;
+        n6.right=n8;
+        n8.left=n9;
+        System.out.print("PREORDER ---> ");
         b1.preOrder(b1.root);
+        System.out.println();
+        System.out.print("POSTORDER ---> ");
+       b1.postOrder(b1.root);
+        System.out.println();
+       System.out.print("INORDER ---> ");
+       b1.inOrder(b1.root);
+
     }
 }
